55c4243e7bf44f98b2dcdf7c0b784a91086b27ec
tefaa1 <mh8369629@gmail.com>
2025-11-24 20:55:12 +0200

    start

---
+++ src/main/java/com/example/Multi_sport_Club_Management_system/MultiSportClubManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Multi_sport_Club_Management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSportClubManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiSportClubManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Multi_sport_Club_Management_system/MultiSportClubManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Multi_sport_Club_Management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSportClubManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
