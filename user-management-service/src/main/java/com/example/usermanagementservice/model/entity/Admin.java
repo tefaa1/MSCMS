c8167b2bfc3d6806dc38809e9ca4fc171c76c286
leo-salem <mohamed01280368366@gmail.com>
2026-02-09 18:23:49 +0200

    update pom.xml,eureka-server,config-server,gateway,user-management-service

---
+++ user-management-service/src/main/java/com/example/usermanagementservice/model/entity/Admin.java
@@ -0,0 +1,15 @@
+package com.example.usermanagementservice.model.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin extends User {
+
+
+}
